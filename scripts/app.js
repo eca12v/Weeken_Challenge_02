910ee050c1590c067c883a3633bec3e9da4c44e3
eca12v <ericanderson@Erics-MacBook-Air.local>
2016-06-04 11:21:18 -0500

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,99 @@
+$( function() {
+
+     $.ajax({
+       url: 'https://raw.githubusercontent.com/devjanaprime/2.4-jQueryAjaxJSON/master/students.json',
+       dataType: 'json',
+       success: function( data ){
+         //declaring global variables and functions for success
+         var x = 0;
+         var studentArray = data.students;
+         var currentInfo = studentArray[x].first_name + ' ' + studentArray[x].last_name + ',  city: ' + studentArray[x].city + ',  shoutout: ' + studentArray[x].shoutout;
+         var changeIndex = function(){
+           if(x === 0){
+             x = 20;
+           }else if(x === 20){
+             x = 0;
+           }
+         };
+         //appending buttons on (documtent).ready and data success
+         var newButtonPrev = document.createElement('button');
+         newButtonPrev.textContent = 'previous';
+         newButtonPrev.id = 'previous';
+         newButtonPrev.className = 'button';
+         var prevButton = new $('#container').append(newButtonPrev);
+
+         for (var i = 0; i < studentArray.length; i++) {
+           var studentName = studentArray[i].first_name;
+           var newButtonStudent = document.createElement('button');
+           newButtonStudent.textContent = studentName;
+           newButtonStudent.id = studentName;
+           newButtonStudent.className = 'button';
+           var studentButton = new $('#container').append(newButtonStudent);
+           console.log(newButtonStudent.className);
+         }
+         var newButtonNext = document.createElement('button');
+         newButtonNext.textContent = 'next';
+         newButtonNext.id = 'next';
+         newButtonNext.className = 'button';
+         var nextButton = new $('#container').append(newButtonNext);
+         $('#studentInfo').html(currentInfo);
+         var timer = setInterval(function(){
+           $('#studentInfo').fadeOut('slow', function(){
+             x++;
+             changeIndex();
+             $('#studentInfo').html(studentArray[x].first_name + ' ' + studentArray[x].last_name + ',  city: ' + studentArray[x].city + ',  shoutout: ' + studentArray[x].shoutout);
+             $('#studentInfo').fadeIn('slow');
+           });
+         }, 10000);
+         function resetInterval() {
+             clearInterval(timer);
+             timer = setInterval(function () {
+               $('#studentInfo').fadeOut('slow', function(){
+                 x++;
+                 changeIndex();
+                 $('#studentInfo').html(studentArray[x].first_name + ' ' + studentArray[x].last_name + ',  city: ' + studentArray[x].city + ',  shoutout: ' + studentArray[x].shoutout);
+                 $('#studentInfo').fadeIn('slow');
+               });
+             }, 10000);
+         }
+
+         //on click event to switch student ino
+         $(document).on('click', '.button', function(){
+           var $this = (this).id;
+           $('#studentInfo').fadeOut('slow', function(){
+             if($this == newButtonPrev.id){
+               changeIndex();
+               x--;
+               $('#studentInfo').html(studentArray[x].first_name + ' ' + studentArray[x].last_name + ',  city: ' + studentArray[x].city + ',  shoutout: ' + studentArray[x].shoutout);
+               $('#studentInfo').fadeIn('slow');
+               resetInterval();
+             } else if ($this == 'next') {
+               x++;
+               changeIndex();
+               $('#studentInfo').html(studentArray[x].first_name + ' ' + studentArray[x].last_name + ',  city: ' + studentArray[x].city + ',  shoutout: ' + studentArray[x].shoutout);
+               $('#studentInfo').fadeIn('slow');
+               resetInterval();
+             } else {
+               for (var i = 0; i < studentArray.length; i++) {
+                 var studentName = studentArray[i].first_name;
+                 if($this == studentName){
+                   $('#studentInfo').html(studentArray[i].first_name + ' ' + studentArray[i].last_name + ',  city: ' + studentArray[i].city + ',  shoutout: ' + studentArray[i].shoutout);
+                   $('#studentInfo').fadeIn('slow');
+                   x = i;
+                 }
+               }
+             }
+            });
+           });
+         }, // end success
+       statusCode: {
+          404: function(){
+             alert( 'error connecting to server' );
+          } // end 404
+        } // end statusCode
+
+
+
+     }); // end ajax  object
+
+  }); // end click getJSONAjax button
